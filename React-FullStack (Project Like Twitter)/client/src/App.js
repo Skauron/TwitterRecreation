1431b833a145af6138ade8079a11d8c562c7f7d7
Momo <43181268+Skauron@users.noreply.github.com>
2024-07-15 16:55:49 -0500

    Auth in the Frontend

--- React-FullStack (Project Like Twitter)/client/src/App.js
+++ React-FullStack (Project Like Twitter)/client/src/App.js
@@ -1,29 +1,62 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useFetcher,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
+import { AuthContext } from "./helpers/AuthContext";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [authState, setAuthState] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/auth/auth", {
+        headers: {
+          accessToken: localStorage.getItem("accessToken"),
+        },
+      })
+      .then((response) => {
+        if (response.data.error) {
+          setAuthState(false);
+        } else {
+          setAuthState(true);
+        }
+      });
+  }, []);
+
   return (
     <div className="App">
-      <Router>
-        <div className="navbar">
-          <Link to="/"> Home Page</Link>
-          <Link to="/createpost"> Create a Post</Link>
-          <Link to="/login"> Login</Link>
-          <Link to="/registration"> Registration</Link>
-        </div>
-        <Routes>
-          <Route path="/" exact Component={Home} />
-          <Route path="/createpost" exact Component={CreatePost} />
-          <Route path="/post/:id" exact Component={Post} />
-          <Route path="/registration" exact Component={Registration} />
-          <Route path="/login" exact Component={Login} />
-        </Routes>
-      </Router>
+      <AuthContext.Provider value={{ authState, setAuthState }}>
+        <Router>
+          <div className="navbar">
+            <Link to="/"> Home Page</Link>
+            <Link to="/createpost"> Create a Post</Link>
+            {!authState && (
+              <>
+                <Link to="/login"> Login</Link>
+                <Link to="/registration"> Registration</Link>
+              </>
+            )}
+          </div>
+          <Routes>
+            <Route path="/" exact Component={Home} />
+            <Route path="/createpost" exact Component={CreatePost} />
+            <Route path="/post/:id" exact Component={Post} />
+            <Route path="/registration" exact Component={Registration} />
+            <Route path="/login" exact Component={Login} />
+          </Routes>
+        </Router>
+      </AuthContext.Provider>
     </div>
   );
 }
